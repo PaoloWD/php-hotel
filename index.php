cd79296e75803685706280e93f5d14a1ae4e7c58
PaoloWD <paolo.lore.93@gmail.com>
2022-12-20 14:20:25 +0100

    create a table with hotels and elements

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    echo var_export($hotels, true)
+
+?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP HOTEL</title>
+ </head>
+ <body>
+    <table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Parking</th>
+            <th scope="col">Vote</th>
+            <th scope="col">Distance to center</th>
+        </tr>
+    </thead>
+    <tbody>
+       <?php 
+        foreach ($hotels as $hotel) {
+            echo "<tr>";
+            echo "<td>" . $hotel["name"] . "</td>";
+            echo "<td>" . $hotel["description"] . "</td>";
+            echo "<td>" . $hotel["parking"] . "</td>";
+            echo "<td>" . $hotel["vote"] . "</td>";
+            echo "<td>" . $hotel["distance_to_center"] . "</td>";
+            echo "</tr>";
+            }
+        ?>
+    </tbody>
+    </table>  
+ </body>
+ </html>
+
